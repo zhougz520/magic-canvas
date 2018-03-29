e03401fb75c89b07239c23a5c59ef0c8d2a55595
ligt <524839917@qq.com>
2018-03-29 17:41:14 +0800

    修改画布

--- src/component/config.ts
+++ src/component/config.ts
@@ -1,7 +1,7 @@
 // 画布的偏移量
 export interface ICompos {
     stageOffset: { top: number, left: number, right: number, bottom: number };  // stage相对body的偏移量
-    canvasOffset: { top: number, left: number };                                // canvas相对stage的偏移量
+    canvasOffset: { top: number, left: number, right: number, bottom: number };  // canvas相对stage的偏移量
 }
 
 export interface IConfig {
@@ -12,6 +12,6 @@ export interface IConfig {
 export const config: IConfig = {
     componentPosition: {
         stageOffset: { top: 80, left: 184, right: 250, bottom: 35 },
-        canvasOffset: { top: 48, left: 48 }
+        canvasOffset: { top: 48, left: 48, right: 48, bottom: 32 }
     }
 };
