b3ebe3eac5f223f1f3fdcd1b82297cc986ca429d
zhougz520 <zhougz520@gmail.com>
2018-05-16 15:58:09 +0800

    把事件监听绑定到document

--- src/component/Canvas/handlers/canvas/docKeyDown.ts
+++ src/component/Canvas/handlers/canvas/docKeyDown.ts
@@ -1,12 +1,12 @@
 import { Canvas } from '../../Canvas';
 import { IKeyArgs } from '../../utils/MouseAndKeyUtil';
 
-export function conKeyDown(canvas: Canvas, e: any): void {
+export function docKeyDown(canvas: Canvas, e: any): void {
     const args = canvas._mouseAndKeyUtil.keyArgs(e);
     const { key, ctrl, alt, keyCode } = args as IKeyArgs;
 
     // 非编辑模式：执行组件删除、组件移动等操作
-    if (canvas._canvasGlobalParam.getIsRichEditMode() === false) {
+    if (canvas._isRichEditMode === false) {
         if (ctrl) {
             canvas._canvasGlobalParam.ctrlPress = true;
         }
@@ -41,18 +41,18 @@ export function conKeyDown(canvas: Canvas, e: any): void {
                 e.preventDefault();
                 break;
         }
-    }
 
-    // 如果是输入操作，进入输入状态
-    if (!ctrl && !alt) {
-        const TECellEditorActivateKeyRange: any = canvas._canvasGlobalParam.getTECellEditorActivateKeyRange();
-        for (let i = 0; i < TECellEditorActivateKeyRange.length; i++) {
-            const { min, max } = TECellEditorActivateKeyRange[i];
-            if (keyCode >= min && keyCode <= max) {
-                // 非编辑模式且有选中组件，进入编辑状态
-                if (canvas._canvasGlobalParam.getIsRichEditMode() === false && canvas._canvasGlobalParam.isSelectedComponent() === true) {
-                    canvas._canvasGlobalParam.setIsRichEditMode(true);
-                    canvas._richEditUtil.beginEdit();
+        // 如果是输入操作，进入输入状态
+        if (!ctrl && !alt) {
+            const TECellEditorActivateKeyRange: any = canvas._canvasGlobalParam.getTECellEditorActivateKeyRange();
+            for (let i = 0; i < TECellEditorActivateKeyRange.length; i++) {
+                const { min, max } = TECellEditorActivateKeyRange[i];
+                if (keyCode >= min && keyCode <= max) {
+                    // 非编辑模式且有选中组件且焦点在僚机上，进入编辑状态
+                    if (canvas._canvasGlobalParam.isSelectedComponent() === true && canvas._isWingmanFocus === true) {
+                        canvas._canvasGlobalParam.setIsRichEditMode(true);
+                        // canvas._richEditUtil.beginEdit();
+                    }
                 }
             }
         }
--- src/component/Canvas/handlers/canvas/docMouseDown.ts
+++ src/component/Canvas/handlers/canvas/docMouseDown.ts
@@ -1,6 +1,6 @@
 import { Canvas } from '../../Canvas';
 
-export function conMouseDown(canvas: Canvas, e: any): void {
+export function docMouseDown(canvas: Canvas, e: any): void {
     if (!canvas._canvasGlobalParam.getIsCanCtrl()) return;
     // 鼠标按下时，计算鼠标位置
     canvas._mouseAndKeyUtil.recordPointStart(e);
@@ -8,6 +8,11 @@ export function conMouseDown(canvas: Canvas, e: any): void {
     // 锚点上点击
     const anchor = canvas._canvasGlobalParam.getCurrentAnchor();
     if (anchor) {
+        // 如果是编辑模式：结束编辑模式
+        if (canvas._isRichEditMode === true) {
+            // canvas._richEditUtil.endEdit();
+            canvas._canvasGlobalParam.setIsRichEditMode(false);
+        }
         // 此处必须阻止事件冒泡，否则可能绘选中覆盖的组件
         e.stopPropagation();
         e.preventDefault();
@@ -21,8 +26,8 @@ export function conMouseDown(canvas: Canvas, e: any): void {
             case 'canvas': {
                 // 画布上的点击
                 // 如果是编辑模式：结束编辑模式
-                if (canvas._canvasGlobalParam.getIsRichEditMode() === true) {
-                    canvas._richEditUtil.endEdit();
+                if (canvas._isRichEditMode === true) {
+                    // canvas._richEditUtil.endEdit();
                     canvas._canvasGlobalParam.setIsRichEditMode(false);
                 }
 
@@ -35,13 +40,9 @@ export function conMouseDown(canvas: Canvas, e: any): void {
 
                 return canvas._canvasGlobalParam.canvasMouseDown(e);
             }
-            // case 'outside': {
-            //     console.log('outside-MouseDown');
-            //     // 外框上
-            //     canvas.clearSelected();
-
-            //     return canvas.command.outsideMouseDown(e);
-            // }
+            case 'outside': {
+                return canvas._canvasGlobalParam.outsideMouseDown(e);
+            }
         }
     }
 }
